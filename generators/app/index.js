e8ab0d8cdc40e90f7433036669f99645f2d30b82
bin lu <lu.bin.lu.bin@gmail.com>
2018-04-11 12:38:04 -0400

    interact with file system

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,22 @@
 var Generator = require('yeoman-generator');
+var beautify = require('gulp-beautify');
+var gulpif = require('gulp-if');
+
+var condition = function (file) {
+    console.log(file.extname);
+    if (file.extname === '.html') {
+        return false;
+    } else {
+        return true;
+    }
+  }
 
 module.exports = class extends Generator {
     // The name `constructor` is important here
     constructor(args, opts) {
         // Calling the super constructor is important so our generator is correctly set up
         super(args, opts);
-  
+
         this.argument('myarg', {
             type: Array,
             required: true
@@ -13,12 +24,14 @@ module.exports = class extends Generator {
 
         // Next, add your custom code
         this.option('babel'); // This method adds support for a `--babel` flag
+
+        this.registerTransformStream(gulpif(condition, beautify({indent_size: 2 })));
     }
 
     method1() {
         this.log('method 1 just ran');
     }
-    
+
     method2() {
         this.log('method 2 just ran');
         this.log(this.options['myarg'])
@@ -45,5 +58,13 @@ module.exports = class extends Generator {
             this.log('app name', answers.name);
             this.log('cool feature', answers.cool);
         });
-      }    
+    }
+
+    writing() {
+        this.fs.copyTpl(
+          this.templatePath('index.html'),
+          this.destinationPath('public/index.html'),
+          { title: this.options['myarg'][1] }
+        );
+    }
   };
